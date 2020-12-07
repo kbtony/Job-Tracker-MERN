6dd449e1fa92376ee861b605071669592c6a5e86
kbtony <tony790511@yahoo.com.tw>
2020-12-07 20:44:02 +1100

    modify routes 2

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -2,10 +2,10 @@
 // this file served as the router file
 
 const router = require('express').Router();
-let Exercise = require('../models/exercise.model');
+let Job = require('../models/exercise.model');
 
 router.route('/').get((req, res) => {
-  Exercise.find()
+  Job.find()
     .then(exercises => res.json(exercises))
     .catch(err => res.status(400).json('Error: ' + err));
 });
@@ -19,7 +19,7 @@ router.route('/add').post((req, res) => {
   const date = Date.parse(req.body.date);
   const status = req.body.status;
 
-  const newExercise = new Exercise({
+  const newExercise = new Job({
     company,
     jobTitle,
     description,
@@ -29,25 +29,25 @@ router.route('/add').post((req, res) => {
   });
 
   newExercise.save()
-  .then(() => res.json('Exercise added!'))
+  .then(() => res.json('Job added!'))
   .catch(err => res.status(400).json('Error: ' + err));
 });
 
 // add some more API endpoints
 router.route('/:id').get((req, res) => {
-  Exercise.findById(req.params.id)
+  Job.findById(req.params.id)
     .then(exercise => res.json(exercise))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/:id').delete((req, res) => {
-  Exercise.findByIdAndDelete(req.params.id)
-    .then(() => res.json('Exercise deleted.'))
+  Job.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Job deleted.'))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/update/:id').post((req, res) => {
-  Exercise.findById(req.params.id)
+  Job.findById(req.params.id)
     .then(exercise => {
       exercise.company = req.body.company;
       exercise.jobTitle = req.body.jobTitle;
@@ -57,7 +57,7 @@ router.route('/update/:id').post((req, res) => {
       exercise.status = req.body.status;
 
       exercise.save()
-        .then(() => res.json('Exercise updated!'))
+        .then(() => res.json('Job updated!'))
         .catch(err => res.status(400).json('Error: ' + err));
     })
     .catch(err => res.status(400).json('Error: ' + err));
